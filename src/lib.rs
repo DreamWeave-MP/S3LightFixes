34543c2d52f73f1a96df4cfa5fc5b7245b23d39b
Dave Corley <corleycomputerrepair@protonmail.ch>
2025-08-07 01:59:04 -0700

    Revert "Improvement: read openmw.cfg from cwd or parent dir"
    This reverts commit 3f9a144df3c022e0e00cfdf32c4add527d0c1054.

--- src/lib.rs
+++ src/lib.rs
@@ -39,23 +39,12 @@ pub fn get_config_path(args: &mut LightArgs) -> PathBuf {
 
         panic!("This shit should never ever happen!");
     } else {
-        let cwd = current_dir()
-            .expect("Failed to get current directory");e
-            
-        let cwd_cfg = cwd
-        .join("openm
-            
-        if let Ok(_) = metadata(&cwd_cfg) {
+        let cwd_cfg = current_dir()
+            .expect("Failed to get current directory")
+            .join("openmw.cfg");
+
+        if cwd_cfg.is_file() {
             return cwd_cfg;
-        }
-        
-        let parent_cfg = cwd
-        .parent()
-        .expect("Failed to get parent directory!")
-        .join("openmw.cfg");
-        
-        if let Ok(_) = metadata(&parent_cfg) {
-            return parent_cfg;
         }
     }
 
